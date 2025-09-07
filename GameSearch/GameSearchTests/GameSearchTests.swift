62c8f57a1e1d34e9dbbdd36c917aee54c8d22437
Joffrey Trn <101974770+MentalDeFeur@users.noreply.github.com>
2025-09-07 21:51:57 +0200

    init app

---
+++ GameSearch/GameSearchTests/GameSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameSearchTests.swift
+//  GameSearchTests
+//
+//  Created by Joffrey TERRINE on 07/09/2025.
+//
+
+import Testing
+@testable import GameSearch
+
+struct GameSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
